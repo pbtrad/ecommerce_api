7d197592abc26707623ae624c532fc203f713fee
paul <paulbrowne2@gmail.com>
2021-12-06 22:03:30 +0000

    added proper api urls

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+from api_app.views import RegistrationAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api_app.urls')),
+    #path('api/v1/auth/auth-token', obtain_auth_token, name='obtain-auth-token')
+    path('auth/register/', RegistrationAPIView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh-token', TokenRefreshView.as_view(), name='refreshtoken'),
 ]
